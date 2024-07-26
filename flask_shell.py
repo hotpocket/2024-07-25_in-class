48c3182f7d33eeac57b1f111f3ea630134cd828a
hotpocket <githotpocket@gmail.com>
2024-07-26 00:37:21 -0700

    dict is quite possibly the worse name that one can choose for a dictionary ... fix that

--- flask_shell.py
+++ flask_shell.py
@@ -5,7 +5,7 @@ from flask import Flask, redirect, request
 
 app = Flask(__name__)
 # a few starter url short codes
-dict = {"good": "https://google.com", "evil": "https://facebook.com"}
+shortCodes = {"good": "https://google.com", "evil": "https://facebook.com"}
 # hard coded valid api key value for testing
 valid_api_key = 'TEST-API-KEY'
 
@@ -16,8 +16,8 @@ valid_api_key = 'TEST-API-KEY'
 @app.route('/<short_code>', methods=['GET']) # should only be valid for a GET
 def allow(short_code):
     print("The short_code was: ", short_code);
-    if short_code in dict:
-        return redirect(dict[short_code], code=301)
+    if short_code in shortCodes:
+        return redirect(shortCodes[short_code], code=301)
     else:
         return 'Your short-code was not found!'
 
@@ -28,12 +28,12 @@ def crud_api_delete(short_code):
     is_valid_api_key = request.headers.get('API-KEY') == valid_api_key
     match request.method:
         case 'GET':
-            if short_code not in dict: return f'Short code: {short_code} was not found!'
-            return f'Short code: {short_code} maps to:{dict[short_code]}'
+            if short_code not in shortCodes: return f'Short code: {short_code} was not found!'
+            return f'Short code: {short_code} maps to:{shortCodes[short_code]}'
         case 'DELETE':
             if(not is_valid_api_key):  return 'API-KEY is not valid!'
-            if short_code not in dict: return f'short-code: {short_code} NOT FOUND'
-            dict.pop(short_code) # delete the url short code
+            if short_code not in shortCodes: return f'short-code: {short_code} NOT FOUND'
+            shortCodes.pop(short_code) # delete the url short code
             return f'Short-code: {short_code} DELETED'
         case 'POST':
             # now that this is shortened an inconsistency is revealed. the code should be provided on the url
@@ -41,8 +41,8 @@ def crud_api_delete(short_code):
             posted = request.json
             url, code = posted['url'], posted['code']
             if url is None or code is None:  return 'You must provide a url and a short-code'
-            op = 'UPDATED' if code in dict else 'CREATED'
-            dict[code] = url
+            op = 'UPDATED' if code in shortCodes else 'CREATED'
+            shortCodes[code] = url
             return f'\nShort-code: {op}.  {code} now points to: {url} \n'
         case _: # default/unmatched case
             return f'{request.method} Not implemented not implemented'
